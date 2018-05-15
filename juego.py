a1f234bf8ed9ad558bce2ab58fadf2144e12eb57
Franco Ramirez <20143156@aloe.ulima.edu.pe>
2018-05-14 21:27:31 -0500

    estrtuctura principal fachada creada

---
+++ juego.py
@@ -0,0 +1,11 @@
+# Importar módulos de pantallas y más
+
+# Definir métodos
+
+# Definir main
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
---
+++ pantallas.py
@@ -0,0 +1,30 @@
+class Pantalla:
+    def get_input(self):
+        pass
+    def update(self):
+        pass
+    def render(self):
+        pass
+
+class PantallaJugador(Pantalla):
+    def ir_menu(self):
+        pass
+    def ir_sala(self):
+        pass
+    def ir_juego(self):
+        pass
+    def ir_escoger_avatar(self):
+        pass
+    def ir_escoger_ropa(self):
+        pass
+
+class PantallaAdmin(Pantalla):
+    def ir_mensage_global(self):
+        pass
+    def ir_configurar_juego(self):
+        pass
+    def ir_configurar_cuenta(self):
+        pass
+    def ir_registrar_cuenta(self):
+        pass
+
