0dffcd90bf0664f7601bcb6c5760543a06b7cfd8
Franco Ramírez Reyes <20143156@aloe.ulima.edu.pe>
2018-05-17 13:55:21 -0500

    password en login y http for humans

--- pantalla_login.py
+++ pantalla_login.py
@@ -9,15 +9,23 @@ class PantallaLogin(pantallas.Pantalla):
             300,
             config.text_input_ancho,
             config.text_input_alto)
+    input_password = text_input.InputBox(
+            (config.ANCHO - config.text_input_ancho) / 2,
+            500,
+            config.text_input_ancho,
+            config.text_input_alto)
     def get_input(self):
         for event in pygame.event.get():
             self.input_usuario.handle_event(event)
+            self.input_password.handle_event(event)
     def update(self):
         self.input_usuario.update()
+        self.input_password.update()
     def render(self):
         self.gestor.superficie.fill(config.BACKGROUND_COLOR)
         self.gestor.pantalla.blit(self.gestor.superficie, (0,0))
         self.input_usuario.draw(self.gestor.pantalla)
+        self.input_password.draw(self.gestor.pantalla)
         pygame.display.update()
     def ir_jugador(self):
         pass
