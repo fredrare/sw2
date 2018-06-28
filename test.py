d8ec7de019847755791e92d04a9e77a1940903b7
Cesar <cesarsan0196@gmail.com>
2018-06-28 18:12:50 -0500

    nuevos botones cambiar equipo y piso

--- test.py
+++ test.py
@@ -2,13 +2,10 @@
 # Importar módulos de pantallas y más
 import pygame
 import config
-<<<<<<< HEAD
 import pantalla_sala
 
-=======
 import pantalla_registro
 #import pantalla_personajes
->>>>>>> 2457de70467621a4e2560f9eb261f8dcf66342ea
 # Definir métodos
 
 # Definir constantes
@@ -19,11 +16,7 @@ class GestorPantallas:
         pygame.init()
         self.pantalla = pygame.display.set_mode(dimensiones)
         self.superficie = pygame.Surface(self.pantalla.get_size())
-<<<<<<< HEAD
         self.pantalla_actual = pantalla_sala.PantallaSala(self)
-=======
-        self.pantalla_actual = pantalla_registro.PantallaRegistro(self)
->>>>>>> 2457de70467621a4e2560f9eb261f8dcf66342ea
 
     def comenzar(self):
         while True:
