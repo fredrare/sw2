4880fdfc97628f5991661c4770b5d8ba0765dddc
Franco Ramírez Reyes <20143156@aloe.ulima.edu.pe>
2018-06-28 13:37:38 -0500

    bala actualizada

--- bala.py
+++ bala.py
@@ -9,34 +9,38 @@ class Bala(pantallas.Pantalla):
 
     def __init__(self, gestor, x, y):
         self.gestor = gestor
-        self.x = x
-        self.y = y
-        self.v = 80
+        self.xinicial = x +65
+        self.direccion = 1
+        self.radio = 10
+        self.x = x + 65
+        self.y = y - self.radio - 70
+        self.v = 150
         self.tiempo = 0
         self.angulo = 45
         self.xmovimiento = x
         self.ymovimiento = config.ANCHO - self.y
         self.disparo = False
-      self.fondo = pygame.image.load("Imagenes/peru.j")
+        self.fondo = pygame.image.load("Imagenes/peru.jpg")
         self.clock = pygame.time.Clock()
         self.fuente = pygame.font.Font(None, 15)
+        self.imagen_otorongo = pygame.image.load("Imagenes/Otorongo.png").convert_alpha()
 
     def update(self):
 
-        self.vx = self.v * math.cos(math.radians(self.angulo))
+        self.vx = self.v * math.cos(math.radians(self.angulo))*self.direccion
         self.vy = self.v * math.sin(math.radians(self.angulo))
 
         if self.disparo == True:
             self.xmovimiento = self.vx * self.tiempo
-            self.ymovimiento = self.vy *self.tiempo + (-9.8*(self.tiempo**4)/2)
-            self.x = self.xmovimiento
+            self.ymovimiento = self.vy *self.tiempo + (-50*(self.tiempo**2)/2)
+            self.x = self.xmovimiento + self.xinicial
             self.y = config.ANCHO - self.ymovimiento
 
         else :
             pass
 
         if (self.x > config.ANCHO) or (self.y > config.ALTO):
-            self.x = 0
+            self.x = self.xinicial
             self.y = config.ALTO
             self.tiempo = 0
             self.disparo = False
@@ -57,18 +61,32 @@ class Bala(pantallas.Pantalla):
                 elif event.key == K_SPACE:
                     if self.v < 150  and self.disparo == False:
                         self.v = self.v + 1
-
                 elif event.key == K_RIGHT:
+                    if self.direccion < 0 and self.disparo == False:
+                        self.direccion = -self.direccion
+                    self.x = self.x + 10
+                    self.xinicial = self.xinicial +10
+                elif event.key == K_LEFT:
+                    if self.direccion > 0 and self.disparo == False:
+                        self.direccion = -self.direccion
+                    self.x = self.x - 10
+                    self.xinicial = self.xinicial -10
+                elif event.key == K_RETURN:
                     self.disparo = True
                 elif event.key == K_ESCAPE:
                     sys.exit()
         if self.disparo == True:
             self.tiempo = self.tiempo + (tick / 1000.0)
     def render(self):
-
         self.gestor.pantalla.blit(self.fondo,(0,0))
-        pygame.draw.circle(self.gestor.pantalla,(155,155,155),(int(self.x),int(self.y)),10)
+        pygame.draw.circle(self.gestor.pantalla,(155,155,155),(int(self.x),int(self.y)),self.radio)
+        if self.direccion > 0  :
+            self.gestor.pantalla.blit(self.imagen_otorongo,(int(self.xinicial-self.radio),config.ALTO-100))
+        else :
+            self.gestor.pantalla.blit(pygame.transform.flip(self.imagen_otorongo,True,False),(int(self.xinicial-self.radio),config.ALTO-100))
+
         pygame.display.update()
+
 
     def ir_login(self):
         pass
