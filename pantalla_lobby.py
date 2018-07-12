1e2d3c3b6a4082b00254a0eb3ed6e2396395d4b9
Franco Ramírez Reyes <20143156@aloe.ulima.edu.pe>
2018-07-12 18:59:18 -0500

    v1.0

--- pantalla_lobby.py
+++ pantalla_lobby.py
@@ -6,16 +6,12 @@ import requests
 import boton
 import pantalla_login
 import pantalla_sala
-<<<<<<< HEAD
 import sys
-=======
->>>>>>> ec347ad4fcdf3d822d9a7f0c78eb5a8bc4844326
 
 class PantallaLobby(pantallas.Pantalla):
     def __init__(self, gestor):
         pygame.display.set_caption("lobby")
         self.gestor = gestor
-<<<<<<< HEAD
         self.fondo = pygame.image.load(config.fondo[0])
         self.sala1 = boton.Button(300, 280, 250, 80, "Sala 1", 80 )
         self.tienda = boton.Button(700, 70, 90, 50, "Tienda")
@@ -45,23 +41,6 @@ class PantallaLobby(pantallas.Pantalla):
         self.sala1.draw(self.gestor.pantalla)
         self.tienda.draw(self.gestor.pantalla)
         self.salir.draw(self.gestor.pantalla)
-=======
-        self.fondo = pygame.image.load("Imagenes/peru.jpg")
-        self.sala = boton.Button((config.ANCHO - 50)/2, 200, 200, 100, "Sala 1")
-    def get_input(self):
-        for event in pygame.event.get():
-            self.sala.handle_event(event)
-
-    def update(self):
-        if self.sala.active:
-            self.sala.active = False
-            self.gestor.pantalla_actual.ir_sala()
-        pass
-    def render(self):
-        self.gestor.pantalla.blit(self.gestor.superficie, (0,0))
-        self.gestor.pantalla.blit(self.fondo, (0, 0))
-        self.sala.draw(self.gestor.pantalla)
->>>>>>> ec347ad4fcdf3d822d9a7f0c78eb5a8bc4844326
         pygame.display.update()
 
     def ir_tienda(self):
